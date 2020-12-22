7494e0c9e77aece1fca01145a7e1aebbd42e3450
EmilieVrd <74055277+EmilieVrd@users.noreply.github.com>
2020-12-22 01:29:54 +0100

    better responsive, xp section added

--- assets/js/script.js
+++ assets/js/script.js
@@ -88,3 +88,19 @@ for (var i = 0; i < btns.length; i++) {
     this.className += " active";
   });
 }
+
+//w3 aussi, accordéon
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
