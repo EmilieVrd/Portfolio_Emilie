b3a31c314a13d10205c60997fa65abe071975467
EmilieVrd <74055277+EmilieVrd@users.noreply.github.com>
2020-12-22 21:37:47 +0100

    modals added
    +better responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -104,3 +104,48 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
+//w3 modal + stackoverflow
+var btn = document.getElementsByClassName("click-to-open");
+for (var i = 0; i < btn.length; i++) {
+  var thisBtn = btn[i];
+  thisBtn.addEventListener("click", function(){
+    var modal = document.getElementById(this.dataset.modal);
+    modal.style.display = "block";
+}, false);
+}
+// Get the button that opens the modal
+
+
+// All page modals
+var modals = document.querySelectorAll('.modal');
+
+// Get the <span> element that closes the modal
+var spans = document.getElementsByClassName("close");
+
+// When the user clicks the button, open the modal
+for (var i = 0; i < btn.length; i++) {
+ btn[i].onclick = function(e) {
+    e.preventDefault();
+    //modal = document.querySelector(e.target.getAttribute("href"));
+    //modal.style.display = "block";
+ }
+}
+
+// When the user clicks on <span> (x), close the modal
+for (var i = 0; i < spans.length; i++) {
+ spans[i].onclick = function() {
+    for (var index in modals) {
+      if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";    
+    }
+ }
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target.classList.contains('modal')) {
+     for (var index in modals) {
+      if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";    
+     }
+    }
+}
